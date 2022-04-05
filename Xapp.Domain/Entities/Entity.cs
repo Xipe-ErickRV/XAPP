c4b6342d3fe24e8543f84c734cbb13f2978daeb5
erickmend <erickgmbarron@gmail.com>
2022-04-04 17:27:42 -0700

    [Dev] EntityClass

---
+++ Xapp.Domain/Entities/Entity.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xapp.Domain.Entities
+{
+    public class Entity
+    {
+        public int Id { get; private set; }
+        public DateTime CreationDate { get; private set; }
+        public DateTime LastUpdate { get; private set; }
+        public bool IsActive { get; private set; }
+        public bool IsDeleted { get; private set; }
+
+        public void CreateEntity()
+        {
+            CreationDate = DateTime.Now;
+            EditEntity();
+            IsActive = true;
+            IsDeleted = false;
+        }
+        public void EditEntity()
+        {
+            LastUpdate = DateTime.Now;
+        }
+
+        public void ChangeStatus()
+        {
+            IsActive = !IsActive;
+            EditEntity();
+        }
+
+        public void Delete()
+        {
+            IsActive = false;
+            IsDeleted = true;
+            EditEntity();
+        }
+    }
+
+}
