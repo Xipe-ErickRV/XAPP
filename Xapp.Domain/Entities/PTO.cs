5592c0b368a0ad956a43630c7d4cfe5c56adc0c1
erickmend <erickgmbarron@gmail.com>
2022-04-04 18:13:41 -0700

    [Dev] User

---
+++ Xapp.Domain/Entities/PTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xapp.Domain.Enums;
+
+namespace Xapp.Domain.Entities
+{
+    public class PTO : Entity
+    {
+
+        public Status Status { get; set; }
+        public int UserId { get; private set; }
+        public virtual User User { get; private set; }
+    }
+}
---
+++ Xapp.Domain/Enums/Status.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xapp.Domain.Enums
+{
+    public enum Status
+    {
+        Pending = 1,
+        BlaBla = 2,
+        BlaBla2 = 3
+    }
+}
