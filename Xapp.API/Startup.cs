533ce3dd826e94f41433ad0c76bd9e78ce987d7b
Xipe-ErickRV <erick@xipetechnology.com>
2022-04-04 18:29:53 -0700

    [Add] EntityFramework

--- Xapp.API/Startup.cs
+++ Xapp.API/Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -11,6 +12,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Xapp.API.Data;
 
 namespace Xapp.API
 {
@@ -26,6 +28,8 @@ namespace Xapp.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DbService>(x => x.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
