716e527bcac68bae5d582c9e9f4c06dd5b365cf2
Erick <erickmendoza@xipetechnology.com>
2022-04-21 17:26:09 -0700

    [Fix] TransferProperties

---
+++ Xapp.API/Migrations/20220422002227_TransferProperties.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Xapp.API.Migrations
+{
+    public partial class TransferProperties : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "Amount",
+                table: "Transfers",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Concept",
+                table: "Transfers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Amount",
+                table: "Transfers");
+
+            migrationBuilder.DropColumn(
+                name: "Concept",
+                table: "Transfers");
+        }
+    }
+}
