c97716843d16ad6d6e9ab7cc18e0dc8e18f9871b
Silvana-Ruiz <silvanadorita@hotmail.com>
2022-04-26 20:08:33 -0700

    [feat] Login View

---
+++ Xapp.Web/wwwroot/js/login_code.js
@@ -0,0 +1,20 @@
+﻿// PopUp de Cambiar Contraseña
+var closeBut = document.getElementsByClassName('close')[0],
+    modal = document.getElementsByClassName('modal-cont')[0],
+    loginBut = document.getElementsByClassName('contra-button')[0];
+
+//close
+function x() {
+    modal.style.display = "none";
+}
+closeBut.onclick = x;
+
+loginBut.onclick = function () {
+    modal.style.display = "block";
+}
+
+window.onclick = function (e) {
+    if (e.target.className === 'modal-cont') {
+        e.target.style.display = 'inline-block';
+    }
+}
