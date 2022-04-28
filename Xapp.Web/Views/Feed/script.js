471f3e3bc84cbfc0b4029627ad87b5f5a30768d9
Carelia Ibarra <A01251463@tec.mx>
2022-04-28 15:47:03 -0700

    Feed Views Edit
    Cambios pequeños a los views de Feed

--- Xapp.Web/Views/Feed/script.js
+++ Xapp.Web/Views/Feed/script.js
@@ -1,11 +1,14 @@
+// function that displays create post modal
 function show_popup() {
     document.getElementById('yeet').style.display = "flex";
 }
 
+// function that closes create post modal
 function close_popup() {
     document.getElementById('yeet').style.display = "none";
 }
 
+// function that changes like icon color when clicked
 function change_like_color(like_id) {
     if (like_id.style.color === "rgb(52, 52, 52)") {
         like_id.style.color = "rgb(226, 36, 50)";
