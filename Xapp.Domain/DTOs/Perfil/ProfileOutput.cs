60152c4b98c6fe13e389180ea9526bb226c63acc
Fernando Mendivil Terminel <fermendivilt@gmail.com>
2022-04-28 17:24:14 -0700

    [DEV] ProfileOutput

---
+++ Xapp.Domain/DTOs/Perfil/ProfileOutput.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xapp.Domain.Entities;
+
+namespace Xapp.Domain.DTOs.Perfil
+{
+    public class ProfileOutput
+    {
+        public string Telefono { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Area { get; set; }
+        public string Bio { get; set; }
+        public DateTime FechaCumple { get; set; }
+        public List<Skill> Skills { get; set; }
+    }
+}
