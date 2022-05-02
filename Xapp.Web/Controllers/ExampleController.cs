09a7ad268348950baa38e8e28f8d7333efce6599
Erick <erickmendoza@xipetechnology.com>
2022-05-02 10:34:55 -0700

    [Dev] Sweet Alerts

--- Xapp.Web/Controllers/ExampleController.cs
+++ Xapp.Web/Controllers/ExampleController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Xapp.Domain.DTOs.Example;
+using Xapp.Web.Utilities;
 
 namespace Xapp.Web.Controllers
 {
@@ -21,8 +22,19 @@ namespace Xapp.Web.Controllers
         {
             //aquí se manda a llamar a los endpoints que estan desarrollando en el proyecto API
             var output = input;
-            return View(output);
-        }
+            //return View(output);
 
+            if (input.Carro == "ok")
+            {
+                return View(output)
+                     .WithSuccess("Transacción completada", "Bienvenido hehen not hehe", "ok");
+            }
+            else
+            {
+                return View(output)
+     .WithDanger("Transacción fallida", "vuelva a intentarlo de nuevo", "ok");
+            }
+
+        }
     }
 }
---
+++ Xapp.Web/Utilities/Alerts.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.Extensions.DependencyInjection;
+using System.Threading.Tasks;
+
+namespace Xapp.Web.Utilities
+{
+    public static class AlertExtensions
+    {
+        private static IActionResult Alert(IActionResult result, string type, string title, string body, string buttonText)
+        {
+            return new AlertDecoratorResult(result, type, title, body, buttonText);
+        }
+
+        public static IActionResult WithSuccess(this IActionResult result, string title, string body, string buttonText )
+        {
+            return Alert(result, "success", title, body, buttonText );
+        }
+
+        public static IActionResult WithDanger(this IActionResult result, string title, string body, string buttonText )
+        {
+            return Alert(result, "error", title, body, buttonText );
+        }
+    }
+    public class AlertDecoratorResult : IActionResult
+    {
+        public IActionResult Result { get; }
+        public string Type { get; }
+        public string Title { get; }
+        public string Body { get; }
+        public string ButtonText { get; }
+
+        public AlertDecoratorResult(IActionResult result, string type, string title, string body, string buttonText)
+        {
+            Result = result;
+            Type = type;
+            Title = title;
+            Body = body;
+            ButtonText = buttonText;
+        }
+
+        public async Task ExecuteResultAsync(ActionContext context)
+        {
+            var factory = context.HttpContext.RequestServices.GetService<ITempDataDictionaryFactory>();
+
+            var tempData = factory.GetTempData(context.HttpContext);
+            tempData["_alert.type"] = Type;
+            tempData["_alert.title"] = Title;
+            tempData["_alert.body"] = Body;
+            tempData["_alert.buttonText"] = ButtonText;
+            await Result.ExecuteResultAsync(context);
+        }
+    }
+}
