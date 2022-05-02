76966addda7c87022b53397b750ac2db7c6d1e4f
Fernando Mendivil Terminel <fermendivilt@gmail.com>
2022-05-02 16:29:21 -0700

    Auth Services for startup

--- Xapp.Web/Startup.cs
+++ Xapp.Web/Startup.cs
@@ -8,6 +8,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Xapp.Web.Services;
 
 namespace Xapp.Web
 {
@@ -23,6 +24,7 @@ namespace Xapp.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.AddScoped<JWTMiddlewareService>();
             services.AddControllersWithViews();
         }
 
